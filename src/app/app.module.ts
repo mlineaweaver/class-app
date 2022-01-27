5bafb44c43609e6173e1427be6f5e5c8567d8623
Melissa Lineaweaver <MelissaHastie16@gmail.com>
2022-01-26 17:57:16 -0700

    Week 2 commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PizzaAppComponent } from './pizza-app/pizza-app.component';
+import { SizePipe } from './shared/pipes/size.pipe';
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent, PizzaAppComponent, SizePipe],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
