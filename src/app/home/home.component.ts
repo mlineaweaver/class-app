3242d70292067bf131357a02b56c700634f55d92
Melissa Lineaweaver <MelissaHastie16@gmail.com>
2022-02-07 23:15:27 -0700

    front-end components/stylings

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { PizzaService } from './../shared/services/pizzas.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  title = 'home!';
+  readonly pizzas$ = this.pizzasService.getPizzaPresets();
+
+  constructor(private pizzasService: PizzaService) {}
 }
+
+
